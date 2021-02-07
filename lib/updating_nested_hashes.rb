14297d95f1cc2bc8bb65e0617e138173ed4fdf0c
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 03:45:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,8 +22,9 @@ def update_status
   
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
-epic_tragedy[:additional_characters] << prince
-epic_tragedy[:additional_characters] << apothecary
+
+epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+epic_tragedy[:additional_characters] << { name: "Apothecary" }
    
    epic_tragedy
    
