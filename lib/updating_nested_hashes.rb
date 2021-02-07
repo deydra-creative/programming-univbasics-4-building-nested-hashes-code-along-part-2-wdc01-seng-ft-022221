057acdeafc5a34c5872357357ebd494cbdfca8a0
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 03:25:26 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,10 +21,6 @@ def update_status
   }
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
-  #code your solution here:
-
-
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  
   epic_tragedy
 end
