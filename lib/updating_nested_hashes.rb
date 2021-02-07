9877c3c8936c0d849ea0b26cfd1df8fb7e11ef81
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 03:16:34 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -52,6 +52,6 @@ def add_characters
       }
    }
 
-   epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
 epic_tragedy[:additional_characters] << { name: "Apothecary" }
 end
