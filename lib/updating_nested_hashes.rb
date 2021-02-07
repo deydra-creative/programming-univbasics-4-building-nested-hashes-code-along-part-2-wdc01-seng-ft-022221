76c5d90b5c734d27a38188403b3fe20ea080e6d5
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 03:48:44 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,32 +1,63 @@
 def update_status
-    epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
-      ]
-   },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"},
-          {name: "Nurse", age: "44", attitude: "worried"}
-      ]
+   epic_tragedy = {
+      :montague => {
+         :patriarch => {name: "Lord Montague", age: "53"},
+         :matriarch => {name: "Lady Montague", age: "54"},
+         :hero => {name: "Romeo", age: "15", status: "alive"},
+         :hero_friends => [
+            {name: "Benvolio", age: "17", attitude: "worried"},
+            {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         ]
+      },
+      :capulet => {
+         :patriarch => {name: "Lord Capulet", age: "50"},
+         :matriarch => {name: "Lady Capulet", age: "51"},
+         :heroine => {name: "Juliet", age: "15", status: "alive"},
+         :heroine_friends => [
+         {name: "Steven", age: "30", attitude: "confused"},
+         {name: "Nurse", age: "44", attitude: "worried"}
+         ]
+      }
    }
-  }
-  
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-epic_tragedy[:additional_characters] = [
-        {name: "Prince Escalus"},
-        {name: "Apothecary"}
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
+  epic_tragedy
+end
+
+def add_characters
+   epic_tragedy = {
+      :montague => {
+         :patriarch => {name: "Lord Montague", age: "53"},
+         :matriarch => {name: "Lady Montague", age: "54"},
+         :hero => {name: "Romeo", age: "15", status: "alive"},
+         :hero_friends => [
+            {name: "Benvolio", age: "17", attitude: "worried"},
+            {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         ]
+      },
+      :capulet => {
+         :patriarch => {name: "Lord Capulet", age: "50"},
+         :matriarch => {name: "Lady Capulet", age: "51"},
+         :heroine => {name: "Juliet", age: "15", status: "alive"},
+         :heroine_friends => [
+         {name: "Steven", age: "30", attitude: "confused"},
+         {name: "Nurse", age: "44", attitude: "worried"}
+         ]
+      }
+   }
+
+   # Write code below that does the following programmatically:
+   #
+   # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
+   # - Add two hashes to this array, both with a :name key.
+   #   - The first hash should have the :name key set to "Prince Escalus" 
+   #   - The second hash should have the :name key set to "Apothecary" 
+   # Code your solution here:
+      epic_tragedy[:additional_characters] = [
+          {name: "Prince Escalus"},
+          {name: "Apothecary"}
           ]
-          
    epic_tragedy
 end
